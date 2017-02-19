422b37a47dc0cf0687615c2546614dd63c8a9eaa
Akhila Devi Devabhaktuni <akhiladevi95@gmail.com>
2017-02-19 12:01:29 +0530

    Update replication-example.R

--- replication-example.R
+++ replication-example.R
@@ -1,6 +1,9 @@
-source("split_merge.R")
-set.seed(123)
-X<-matrix(rnorm(50000),ncol = 100,nrow = 500,byrow = T)
-usual.svd<-svd(X)
-new.ago<-split_merge(X)
-l<-lineprof(split_merge(X))
+replicatio_function <- function(){
+  source("split_merge.R")
+  set.seed(123)
+  X<-matrix(rnorm(50000),ncol = 100,nrow = 500,byrow = T)
+  usual.svd<-svd(X)
+  new.ago<-split_merge(X)
+  l<-lineprof(split_merge(X))
+  return(list("usvd"<-usual.svd,"nago"<-new.ago,"l1"<-l))
+}
